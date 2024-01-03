4a197518ed1dbd2236d2fcbb74141194681b7cf0
Jim Porter <jporterbugs@gmail.com>
2024-01-03 11:14:14 -0800

    Update copyright

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-# Copyright (C) 2023 Jim Porter
+# Copyright (C) 2022-2024 Jim Porter
 
 # This file is NOT part of GNU Emacs.
 
--- git-command-modes-tests.el
+++ git-command-modes-tests.el
@@ -1,6 +1,6 @@
 ;;; git-command-modes-tests.el --- Tests for git-command-modes -*- lexical-binding: t -*-
 
-;; Copyright (C) 2023 Jim Porter
+;; Copyright (C) 2022-2024 Jim Porter
 
 ;; Author: Jim Porter
 ;; URL: https://github.com/jimporter/git-command-modes
